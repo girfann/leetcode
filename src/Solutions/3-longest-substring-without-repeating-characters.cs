6eac6f2d5c2da774236c4c436e4208c2d32fd6fb
mark <dbgljk.ff@outlook.com>
2025-08-05 22:48:16 +0500

    Add "3. Longest Substring Without Repeating Characters"

---
+++ src/Solutions/3-longest-substring-without-repeating-characters.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Solutions
+{
+    public partial class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            int length = 0;
+            int startIndex = 0;
+            var indexByChar = new Dictionary<char, int>();
+
+            for (int endIndex = 0; endIndex < s.Length; endIndex++)
+            {
+                if (indexByChar.TryGetValue(s[endIndex], out var charLastIndex) && charLastIndex >= startIndex)
+                    startIndex = charLastIndex + 1;
+
+                indexByChar[s[endIndex]] = endIndex;
+
+                var windowLength = endIndex - startIndex + 1;
+
+                if (windowLength > length)
+                    length = windowLength;
+            }
+
+            return length;
+        }
+    }
+}
