bec6b015c08f37ce511e240e0626d728e55e6cdd
mark <dbgljk.ff@outlook.com>
2025-08-15 14:48:12 +0500

    Refactoring "4. Median of Two Sorted Arrays"

--- src/Solutions/4-median-of-two-sorted-arrays.cs
+++ src/Solutions/4-median-of-two-sorted-arrays.cs
@@ -4,23 +4,23 @@
     {
         public double FindMedianSortedArrays(int[] nums1, int[] nums2)
         {
-            var totalElementsCount = nums1.Length + nums2.Length;
-            var medianArrCount = (totalElementsCount / 2) + 1;
+            var totalCount = nums1.Length + nums2.Length;
+            var totalMedian = (totalCount / 2) + 1;
 
             var current = 0;
             var previous = 0;
 
-            for (int i1 = 0, i2 = 0; (i1 + i2) < medianArrCount;)
+            for (int i1 = 0, i2 = 0; (i1 + i2) < totalMedian;)
             {
-                var takeFirst =
+                var takeFromFirstArray =
                     (i2 >= nums2.Length) ||
                     (i1 < nums1.Length && nums1[i1] < nums2[i2]);
 
                 previous = current;
-                current = takeFirst ? nums1[i1++] : nums2[i2++];
+                current = takeFromFirstArray ? nums1[i1++] : nums2[i2++];
             }
 
-            var isEven = totalElementsCount % 2 == 0;
+            var isEven = totalCount % 2 == 0;
             return isEven ? (previous + current) / 2.0 : current;
         }
     }
