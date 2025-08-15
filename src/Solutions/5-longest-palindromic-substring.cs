4b1582c88199d1357a936e36c62c1beb6559048d
mark <dbgljk.ff@outlook.com>
2025-08-15 17:35:22 +0500

    Add "5. Longest Palindromic Substring"

---
+++ src/Solutions/5-longest-palindromic-substring.cs
@@ -0,0 +1,50 @@
+﻿namespace Solutions
+{
+    public partial class Solution
+    {
+        public string LongestPalindrome(string str)
+        {
+            if (string.IsNullOrEmpty(str) || str.Length == 1)
+                return str;
+
+            var longestPalindrome = string.Empty;
+
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                var index = i;
+                var currentStr = str[i].ToString();
+
+                do
+                {
+                    index++;
+
+                    if (IsPalindrome(currentStr) && currentStr.Length > longestPalindrome.Length)
+                        longestPalindrome = currentStr;
+
+                    if (index == str.Length)
+                        break;
+
+                    currentStr += str[index];
+                }
+                while (index < str.Length);
+            }
+
+            return longestPalindrome;
+        }
+
+        private bool IsPalindrome(string str)
+        {
+            if (str.Length == 1)
+                return true;
+
+            var left = 0;
+            var right = str.Length - 1;
+
+            while (left < right)
+                if (str[left++] != str[right--])
+                    return false;
+
+            return true;
+        }
+    }
+}
